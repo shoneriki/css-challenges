4eb5587dbfb165aea2d07325b55183efcbdb4402
shoneriki <ikirenohs@gmail.com>
2021-05-10 16:23:00 +0900

    deleted spaces in JS file

--- Frontend Masters/Calculator Challenge/script.js
+++ Frontend Masters/Calculator Challenge/script.js
@@ -3,8 +3,6 @@ let buffer = "0";
 let previousOperator = null;
 const screen = document.querySelector('.screen')
 
-
-
 document.querySelector('.calc-buttons').addEventListener('click', function(event) {
   buttonClick(event.target.innerText);
 })
