a5f4fc16ed0d499a75546d2069e29a7666138c96
shoneriki <ikirenohs@gmail.com>
2021-05-10 15:41:18 +0900

    buttons put numbers on screen

--- Frontend Masters/Calculator Challenge/script.js
+++ Frontend Masters/Calculator Challenge/script.js
@@ -1,6 +1,35 @@
-const input = document.querySelector('.input')
-const key = document.querySelector('.key')
-key.addEventListener('click', {
-  input = `${key.innerHTML}`
-  
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector('.screen')
+
+
+
+document.querySelector('.calc-buttons').addEventListener('click', function(event) {
+  buttonClick(event.target.innerText);
 })
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+  rerender();
+}
+
+function handleSymbol(value) {
+
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
