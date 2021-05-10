db336250d725b58e7b18e326ba35c2f148cac759
shoneriki <ikirenohs@gmail.com>
2021-05-10 15:56:46 +0900

    clear and delete works

--- Frontend Masters/Calculator Challenge/script.js
+++ Frontend Masters/Calculator Challenge/script.js
@@ -1,6 +1,6 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 const screen = document.querySelector('.screen')
 
 
@@ -15,6 +15,7 @@ function buttonClick(value) {
   } else {
     handleNumber(value);
   }
+  rerender();
 }
 
 function handleNumber(value) {
@@ -27,8 +28,34 @@ function handleNumber(value) {
 }
 
 function handleSymbol(value) {
-
+  switch (value) {
+    case 'C':
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break
+  }
 }
+
 
 function rerender() {
   screen.innerText = buffer;
